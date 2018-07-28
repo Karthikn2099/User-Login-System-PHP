1d453119066bc2219ab3e5a949edf14614d009a4
Karthikn2098 <karthikn2099@gmail.com>
2018-07-28 22:00:52 +0530

    second commit

--- USER/user-profile.php
+++ USER/user-profile.php
@@ -15,33 +15,6 @@
     header('Location: ../index.php');
   }
 
-  // listening for book-room submit event
-  if( isset($_POST['book-room']) ) {
-    $check_in_date = $_POST['check-in-date'];
-    $check_out_date = $_POST['check-out-date'];
-    $no_of_people = $_POST['no-of-people'];
-    $no_of_rooms = $_POST['no-of-rooms'];
-    $room_type = $_POST['room-type'];
-    $phone = $_POST['phone'];
-
-    if( empty($check_in_date) || empty($check_out_date) || empty($no_of_people) || empty($no_of_rooms) || empty($room_type) || empty($phone) ) {
-      echo "<script>alert('Please fill in all the fields');</script>";
-      header('Location: user-profile.php');
-    }
-    else {
-
-      $insert_user_details_query = "INSERT INTO reserved_rooms_table(NAME ,	EMAIL ,	PHONE ,	CHECK_IN_DATE ,	CHECK_OUT_DATE , NO_OF_PEOPLE ,	NO_OF_ROOMS ,	ROOM_TYPE) VALUES( '$name' , '$email' , '$phone' , '$check_in_date' , '$check_out_date' , '$no_of_people' , '$no_of_rooms' , '$room_type' );";
-      $insert_user_details_result = mysqli_query( $connect , $insert_user_details_query );
-
-      if( $insert_user_details_result ) {
-        sleep(1);
-        header('Location: user-profile.php');
-      }
-
-    }
-
-  }
-
 ?>
 
 <!DOCTYPE html>
@@ -57,7 +30,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
   <link href="https://fonts.googleapis.com/css?family=Paytone+One" rel="stylesheet">
 </head>
-<body onload="openUserTab(event , 'book-room')" >
+<body>
 
   <div id="header">
     <header>
